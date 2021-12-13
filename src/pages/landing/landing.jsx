c3d5af70838c88a28004c1d47456625172996ae2
JuniorTheF <fsmart2006@gmail.com>
2021-12-13 20:20:00 +0300

    refactor footer and header

--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,12 +1,6 @@
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import Header from "../../features/Header"; 
+import Footer from '../../features/Footer'
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
-import Avatar from "@mui/material/Avatar";
-
 import "./landing.css"
 
 
@@ -14,25 +8,7 @@ const Landing = () => {
 	return (
 		<div className="l">
 			<div className="l-top">
-			<Box sx={{ flexGrow: 1 }}>
-				<AppBar position='static' color='primary'>
-					<Toolbar>
-						<IconButton
-							size='large'
-							edge='start'
-							color='inherit'
-							aria-label='menu'
-							sx={{ mr: 2 }}
-						>
-							<MenuIcon />
-						</IconButton>
-						<Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
-							EX-TVS
-						</Typography>
-						<Avatar>Ф</Avatar>
-					</Toolbar>
-				</AppBar>
-			</Box>
+				<Header />
 			</div>
 			<div className="l-body">
 				<div className="l-main">
@@ -52,17 +28,7 @@ const Landing = () => {
 					</div>
 				</div>
 			</div>
-			
-			<footer>
-				<div className="l-footer">
-					<div className="l-year">
-						2021г-2022г
-					</div>
-					<div className="l-prepared">
-						Проект подготовили: Бабушкин Тимофей 10Д, Федор Мартынов 10Д, Николаев Евгений 10А
-					</div>
-				</div>
-			</footer>
+			<Footer />			
 		</div>
 	);
 };
