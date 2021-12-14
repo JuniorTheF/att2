bcf6db7e6142597cda7cb8ddbfcae7a1fcb9ef43
Tim00tiM <olimprofl@gmail.com>
2021-12-14 18:26:44 +0300

    v0.1.5

--- src/pages/login/login.jsx
+++ src/pages/login/login.jsx
@@ -1,66 +1,145 @@
-import Header from '../../features/Header'
-import Footer from '../../features/Footer'
-import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-import Button from '@mui/material/Button';
-
-
-import "./login.css"
-import { margin } from '@mui/system';
+import Header from "../../features/Header";
+import Footer from "../../features/Footer";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import InputAdornment from "@mui/material/InputAdornment";
+import { useState } from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import IconButton from "@mui/material/IconButton";
+import "./login.css";
+import { margin } from "@mui/system";
 
 const Login = () => {
-    return (
-        
-        <div className="log">
-            <Header/>
+  const handleChange = (prop) => (event) => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
 
-                <Box className = "log-face">
-                    <div className="log-text-reg">
-                        Регистрация нового аккаунта
-                    </div>
-                    <div className="log-field">
-                        <div className="log-field-text">
-                           Имя пользователя
-                       </div>
-                       <div class="log-field-data">
-                        <TextField variant="outlined"  fullWidth></TextField>
-                        </div>
-                    </div>
-                    <div className="log-field">
-                        <div className="log-field-text">
-                           Почта
-                       </div>
-                       <div class="log-field-data">
-                        <TextField variant="outlined"  fullWidth></TextField>
-                        </div>
-                    </div>
-                    <div className="log-field">
-                        <div className="log-field-text">
-                           Пароль
-                       </div>
-                       <div class="log-field-data">
-                        <TextField variant="outlined"  fullWidth></TextField>
-                        </div>
-                    </div>
-                    <div className="log-field">
-                        <div className="log-field-text">
-                           Подтверждение пароля
-                       </div>
-                       <div class="log-field-data">
-                        <TextField variant="outlined"  fullWidth></TextField>
-                        </div>
-                    </div>
-                    <div className="log-login-title">Уже есть аккаунт?</div>
-                    <div className="log-button-table">
-                    <Button variant="contained" className="log-button" sx={{ fontSize: 14 }}>Зарегистрироваться</Button>
-                    <Button variant="contained" className="log-button" sx={{ fontSize: 14 }}>Войти</Button>
-                    </div>
-                </Box>
+  const handleClickShowPassword1 = () => {
+    setValues({
+      ...values,
+      showPassword1: !values.showPassword1,
+    });
+  };
+  const handleClickShowPassword2 = () => {
+    setValues({
+      ...values,
+      showPassword2: !values.showPassword2,
+    });
+  };
 
-            <Footer/>
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
+
+  const [values, setValues] = useState({
+    password1: "",
+    password2: "",
+    showPassword1: false,
+    showPassword2: false,
+  });
+
+  return (
+    <div className="log">
+      <Header />
+
+      <Box className="log-face">
+        <div className="log-text-reg">Регистрация нового аккаунта</div>
+        <div className="log-field">
+          <div className="log-field-text">Имя пользователя</div>
+          <div class="log-field-data">
+            <TextField variant="outlined" fullWidth></TextField>
+          </div>
         </div>
+        <div className="log-field">
+          <div className="log-field-text">Почта</div>
+          <div class="log-field-data">
+            <TextField variant="outlined" fullWidth></TextField>
+          </div>
+        </div>
+        <div className="log-field">
+          <div className="log-field-text">Пароль</div>
+          <div class="log-field-data">
+            <FormControl variant="outlined" fullWidth>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword1 ? "text" : "password"}
+                value={values.password1}
+                onChange={handleChange("password1")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword1}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword1 ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </div>
+        </div>
+        <div className="log-field">
+          <div className="log-field-text">Подтверждение пароля</div>
+          <div class="log-field-data">
+            <FormControl variant="outlined" fullWidth>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword2 ? "text" : "password"}
+                value={values.password2}
+                onChange={handleChange("password2")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword2}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword2 ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
+          </div>
+        </div>
+        <div className="log-login-title">Уже есть аккаунт?</div>
+        <div className="log-button-table">
+          <Button
+            variant="contained"
+            className="log-button"
+            sx={{ fontSize: 14 }}
+          >
+            Зарегистрироваться
+          </Button>
+          <Button
+            variant="contained"
+            className="log-button"
+            sx={{ fontSize: 14 }}
+          >
+            Войти
+          </Button>
+        </div>
+      </Box>
 
-    )
-}
+      <Footer />
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/registration/registration.jsx
+++ src/pages/registration/registration.jsx
@@ -3,9 +3,36 @@ import Footer from '../../features/Footer'
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import FormControl from "@mui/material/FormControl";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import InputAdornment from "@mui/material/InputAdornment";
+import { useState } from "react";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import IconButton from "@mui/material/IconButton";
 import "./registration.css"
 
-function registration() {
+function Registration() {
+    const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
+      const handleClickShowPassword1 = () => {
+        setValues({
+          ...values,
+          showPassword1: !values.showPassword1,
+        });
+      };
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
+    
+    
+    
+    const [values, setValues] = useState({
+        password1: "",
+        showPassword1: false
+      });
+    
     return (
         <div classname = "reg">
             <Header/>
@@ -27,7 +54,30 @@ function registration() {
                             Ваш пароль
                         </div>
                         <div className = "reg-field-data">
-                            <TextField variant="outlined"  fullWidth></TextField>
+                        <FormControl variant="outlined" fullWidth>
+              <OutlinedInput
+                id="outlined-adornment-password"
+                type={values.showPassword1 ? "text" : "password"}
+                value={values.password1}
+                onChange={handleChange("password1")}
+                endAdornment={
+                  <InputAdornment position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword1}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {values.showPassword1 ? (
+                        <VisibilityOff />
+                      ) : (
+                        <Visibility />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                }
+              />
+            </FormControl>
                         </div>
                     </div>
                     <div className="reg-button-table">
@@ -41,4 +91,4 @@ function registration() {
     )
 }
 
-export default registration
+export default Registration
